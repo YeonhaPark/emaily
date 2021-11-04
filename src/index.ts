9db6120f9a1799a0fb048b89a65c13e82377fe33
Park Yeonha <bag-yeonha@bag-yeonhaui-MacBookAir.local>
2021-11-04 18:18:17 +0900

    cod change

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,37 @@
 import express from "express";
-import mongoose from "mongoose";
-import { authRoutes } from "./routes/authRoutes";
-import { mongoURI, cookieKey } from "../keys";
-import cookieSession from "cookie-session"; // give access to cookie
-import passport from "passport"; // tell passport to use cookie
-import "./models/User";
-import "./services/passport";
+// import mongoose from "mongoose";
+// import { authRoutes } from "./routes/authRoutes";
+// import { mongoURI, cookieKey } from "../keys";
+// import cookieSession from "cookie-session"; // give access to cookie
+// import passport from "passport"; // tell passport to use cookie
+// import "./models/User";
+// import "./services/passport";
 
-mongoose.connect(mongoURI);
+// mongoose.connect(mongoURI);
 const app = express();
-mongoose.connection.on("connected", () => {
-  console.log("connected successfully");
-});
+// mongoose.connection.on("connected", () => {
+//   console.log("connected successfully");
+// });
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
 
-app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000, // how long this cookie could exist in browser
-    keys: [cookieKey],
-  })
-);
+// app.use(
+//   cookieSession({
+//     maxAge: 30 * 24 * 60 * 60 * 1000, // how long this cookie could exist in browser
+//     keys: [cookieKey],
+//   })
+// );
 
-// tell passport to handle cookies and authentication
-app.use(passport.initialize());
-app.use(passport.session());
+// // tell passport to handle cookies and authentication
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-authRoutes(app);
+// authRoutes(app);
 
-app.use("*", (req, res) => {
-  res.send("<h1>Welcome to your simple server!!</h1>");
-});
+// app.use("*", (req, res) => {
+//   res.send("<h1>Welcome to your simple server!!</h1>");
+// });
 
 const PORT: string | number = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`hosting @${PORT}`));
