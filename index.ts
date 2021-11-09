889b78d35e0a6b33f1368e5f39c60ffcf1dcf0b2
Park Yeonha <bag-yeonha@bag-yeonhaui-MacBookAir.local>
2021-11-10 08:51:26 +0900

    setup mongoose, passport js, auth routes and model

--- index.ts
+++ index.ts
@@ -1,21 +1,19 @@
 import express from "express";
-// import mongoose from "mongoose";
-// import { authRoutes } from "./routes/authRoutes";
-// import { mongoURI, cookieKey } from "../keys";
+import mongoose from "mongoose";
+import { authRoutes } from "./routes/authRoutes";
+import { mongoURI, cookieKey } from "./config/keys";
 // import cookieSession from "cookie-session"; // give access to cookie
-// import passport from "passport"; // tell passport to use cookie
-// import "./models/User";
-// import "./services/passport";
+import "./models/User";
+import "./services/passport"; // 항상 models/User뒤에!
 
-// mongoose.connect(mongoURI);
+mongoose.connect(mongoURI);
 const app = express();
-// mongoose.connection.on("connected", () => {
-//   console.log("connected successfully");
-// });
+mongoose.connection.on("connected", () => {
+  console.log("connected successfully");
+});
 app.get('/', function (req, res) {
   res.send('hello world')
 })
-
 // app.use(
 //   cookieSession({
 //     maxAge: 30 * 24 * 60 * 60 * 1000, // how long this cookie could exist in browser
@@ -27,7 +25,7 @@ app.get('/', function (req, res) {
 // app.use(passport.initialize());
 // app.use(passport.session());
 
-// authRoutes(app);
+authRoutes(app);
 
 // app.use("*", (req, res) => {
 //   res.send("<h1>Welcome to your simple server!!</h1>");
--- services/passport.ts
+++ services/passport.ts
@@ -1,19 +1,19 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import mongoose from "mongoose";
-import { googleAuthParams } from "../../keys";
+import { googleAuthParams } from "../config/keys";
 const { googleClientID, googleClientSecret } = googleAuthParams;
 
 type UserType = { id: string; googleId?: string };
 
 const User = mongoose.model("users");
 
-// 해당하는 유저를 찾았을때 해당 유저의 정보가 밑의 user 아규먼트로 넘어옴.
-passport.serializeUser((user: UserType, done) => done(null, user.id));
+// // 해당하는 유저를 찾았을때 해당 유저의 정보가 밑의 user 아규먼트로 넘어옴.
+// passport.serializeUser((user: UserType, done) => done(null, user.id));
 
-passport.deserializeUser((id, done) => {
-  User.findById(id).then((user) => done(null, user));
-});
+// passport.deserializeUser((id, done) => {
+//   User.findById(id).then((user) => done(null, user));
+// });
 
 passport.use(
   new GoogleStrategy(
@@ -23,6 +23,7 @@ passport.use(
       callbackURL: "/auth/google/callback",
     },
     (accessToken: string, refreshToken: string, profile: any, done: any) => {
+      console.log({accessToken, refreshToken, profile, done})
       User.findOne({ googleId: profile.id }).then((existingUser) => {
         if (existingUser) {
           done(null, existingUser);
