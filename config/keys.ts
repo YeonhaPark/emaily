7ec710daf34517d77ca05b3466d3b1e61c58f45e
Park Yeonha <bag-yeonha@bag-yeonhaui-MacBookAir.local>
2021-11-11 10:01:29 +0900

    console

--- config/keys.ts
+++ config/keys.ts
@@ -8,7 +8,7 @@ googleClientId = process.env.GOOGLE_CLIENT_ID || '';
 googleClientSecret = process.env.GOOGLE_CLIENT_SECRET || '';
 mongoURI = process.env.MONGO_URI || '';
 cookieKey = process.env.COOKIE_KEY || '';
-
+console.log('googleClientId::', googleClientId)
 export {
   googleClientId,
   googleClientSecret,
