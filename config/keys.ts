706015af6e2164c215c2fea45553f4e486b040a8
Park Yeonha <bag-yeonha@bag-yeonhaui-MacBookAir.local>
2021-11-11 09:50:24 +0900

    fix type error

--- config/keys.ts
+++ config/keys.ts
@@ -2,12 +2,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-let googleClientId: string, googleClientSecret: string, mongoURI: string | undefined, cookieKey: string | undefined;
+let googleClientId: string, googleClientSecret: string, mongoURI: string, cookieKey: string;
 
 googleClientId = process.env.GOOGLE_CLIENT_ID || '';
 googleClientSecret = process.env.GOOGLE_CLIENT_SECRET || '';
-mongoURI = process.env.MONGO_URI;
-cookieKey = process.env.COOKIE_KEY;
+mongoURI = process.env.MONGO_URI || '';
+cookieKey = process.env.COOKIE_KEY || '';
 
 export {
   googleClientId,
